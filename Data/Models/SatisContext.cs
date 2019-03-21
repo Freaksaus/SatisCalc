f03abb612ac1cab5a185429b1b700cc3c14e6243
Freaksaus <f.buurman@gmail.com>
2019-03-21 01:54:04 +0100

    Added context and item model

---
+++ Data/Models/SatisContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models
+{
+    public class SatisContext : DbContext
+    {
+        public SatisContext(DbContextOptions<SatisContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
